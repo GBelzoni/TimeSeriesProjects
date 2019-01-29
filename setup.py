d1608d4b248c486b943e89adb9b7af8f5b4c04cb
WIAUS\costelp <patrick.costello@mail.weir>
2019-01-30 07:50:30 +1100

    added minimal package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='statespacepy',
+      version='0.1',
+      description='Basic Statespace models to compare with packages',
+      url='https://github.com/GBelzoni/TimeSeriesProjects',
+      author='Patrick Costello',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['statespacepy'],
+      zip_safe=False)
